1c37bd3a95f478b975b1cc9d84b37f9e5b907bc7
leonel gonzalez <133721483+LeoGittt@users.noreply.github.com>
2024-12-20 16:42:05 -0300

    ELM-27-HELP
    Seccion de ayuda

---
+++ frontend/front/src/app/Ayuda/page.tsx
@@ -0,0 +1,10 @@
+import HelpSection from '@/components/HelpSection'
+
+export default function AyudaPage() {
+  return (
+    <div className="min-h-screen bg-[#edede9]">
+      <HelpSection />
+    </div>
+  )
+}
+
